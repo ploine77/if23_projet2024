dfab8903a8b57ff96fc47b3d8bb306856b79c595
ploine77 <109166168+ploine77@users.noreply.github.com>
2024-03-15 10:48:42 +0100

    Update sort_wifi_data.py
    update code to save the area where the position was taken

--- sort_wifi_data.py
+++ sort_wifi_data.py
@@ -4,32 +4,39 @@ import os
 
 
 text = ""
-print("Quit with q")
 while (text != "q"):
     ## Save current position on file named "doc.json"
-    docPath = r'C:\Users\nono\if23_projet2024\doc.json'
+    docPath = r'C:\Users\nono\Documents\GitHub\if23_projet2024\doc.json'
     if (not os.path.exists(docPath)):
+        oldArea = 0
         oldPoint = 0
         oldVersion = 0
-        with open(r"C:\Users\nono\if23_projet2024\doc.json","w") as fichier:
+        with open(r"C:\Users\nono\Documents\GitHub\if23_projet2024\doc.json","w") as fichier:
             info = {
+                "currentArea": oldArea,
                 "currentPoint": oldPoint,
                 "currentVersion": oldVersion
             }
             json.dump(info, fichier)
     else :
-        with open(r"C:\Users\nono\if23_projet2024\doc.json","r") as fichier:
+        with open(r"C:\Users\nono\Documents\GitHub\if23_projet2024\doc.json","r") as fichier:
             data = json.loads(fichier.read())
+            oldArea = data['currentArea']
             oldPoint = data['currentPoint']
             oldVersion = data['currentVersion']
 
 
     ## Create document if they don't exist
-    dataPath = r'C:\Users\nono\if23_projet2024\data'
-    pointPath = r"C:\Users\nono\if23_projet2024\data\position_{}".format(oldPoint)
+    dataPath = r'C:\Users\nono\Documents\GitHub\if23_projet2024\data'
+    areaPath = r"C:\Users\nono\Documents\GitHub\if23_projet2024\data\area_{}".format(oldArea)
+    pointPath = r"C:\Users\nono\Documents\GitHub\if23_projet2024\data\area_{}\position_{}".format(oldArea,oldPoint)
 
     if (not os.path.exists(dataPath)):
         os.mkdir(dataPath)
+        os.mkdir(areaPath)
+        os.mkdir(pointPath)
+    elif (not os.path.exists(areaPath)):
+        os.mkdir(areaPath)
         os.mkdir(pointPath)
     elif (not os.path.exists(pointPath)):
         os.mkdir(pointPath)
@@ -39,20 +46,35 @@ while (text != "q"):
     iface = profile.interfaces()[0]
     iface.scan()
     results = iface.scan_results()
-    print("Current point : " + str(oldPoint))
-    text = str(input("Do you want to change position (y/n) ? "))
-    if (text == "y"):
+    print("Current area : " + str(oldArea))
+    print("Current position : " + str(oldPoint))
+    print("Current version : " + str(oldVersion))
+    text = str(input("Do you want to change position (y/n), change area (c) or quit (q) ? "))
+    if (text == "c"):
+        currentArea = oldArea + 1
+        currentPoint = 0
+        currentVersion = 1
+        textC = str(input("Launch with ???"))
+        areaPath = r"C:\Users\nono\Documents\GitHub\if23_projet2024\data\area_{}".format(currentArea)
+        pointPath = r"C:\Users\nono\Documents\GitHub\if23_projet2024\data\area_{}\position_{}".format(currentArea,currentPoint)
+        os.mkdir(areaPath)
+        os.mkdir(pointPath)
+    elif (text == "y"):
+        currentArea = oldArea
         currentPoint = oldPoint + 1
         currentVersion = 1
-        pointPath = r"C:\Users\nono\if23_projet2024\data\position_{}".format(currentPoint)
+        pointPath = r"C:\Users\nono\Documents\GitHub\if23_projet2024\data\area_{}\position_{}".format(currentArea,currentPoint)
         os.mkdir(pointPath)
     elif(text == "n"):
+        currentArea = oldArea
         currentPoint = oldPoint
-        currentVersion= oldVersion+1
+        currentVersion = oldVersion + 1
         print(currentVersion)
-    if (text == "y" or "n"):
-        with open(r"C:\Users\nono\if23_projet2024\doc.json","w") as fichier:
+
+    if (text == "y" or "n" or "c"):
+        with open(r"C:\Users\nono\Documents\GitHub\if23_projet2024\doc.json","w") as fichier:
                 info = {
+                    "currentArea": currentArea,
                     "currentPoint": currentPoint,
                     "currentVersion": currentVersion
                 }
@@ -60,14 +82,12 @@ while (text != "q"):
                 json.dump(info, fichier)
 
 
-        versionPath = r"C:\Users\nono\if23_projet2024\data\position_{}\version_{}.json".format(currentPoint,currentVersion)
+        versionPath = r"C:\Users\nono\Documents\GitHub\if23_projet2024\data\area_{}\position_{}\version_{}.json".format(currentArea,currentPoint,currentVersion)
         datas = {}
-        count=0
         for data in results:
-            info = {'ssid': data.ssid,'bssid': data.bssid,'signal': data.signal}
-            if info not in datas.values():
-                datas[count] = info
-                count+=1
+            bssid = data.bssid
+            signal = data.signal
+            datas[bssid] = signal
 
         with open(versionPath, "w") as fichier:
             json.dump(datas, fichier)
